a9cd7003ef511dccbfd3c321670f37591b2b57bc
AngelStoyanov33 <aststoyanov@gmail.com>
2021-04-10 11:01:28 +0300

    Added the HTTPRequestManager class to send requests to the web server

---
+++ app/src/main/java/com/smartsocket/app/service/HTTPRequestManager.java
@@ -0,0 +1,39 @@
+package com.smartsocket.app.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class HTTPRequestManager {
+
+    public static final String SERVER_LOCATION = "http://192.168.0.105:8080";
+
+    public String sendJSONRequest(final String URL, String jsonInputString) throws IOException {
+        HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(URL).openConnection();
+        httpURLConnection.setRequestMethod("POST");
+        httpURLConnection.setRequestProperty("Content-Type", "application/json; utf-8");
+        httpURLConnection.setRequestProperty("Accept", "application/json");
+        httpURLConnection.setDoOutput(true);
+
+        try(OutputStream os = httpURLConnection.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
+
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(httpURLConnection.getInputStream(), StandardCharsets.UTF_8))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            return response.toString();
+        }
+
+    }
+
+}
